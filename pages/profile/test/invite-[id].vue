11030ab4432510f25c62e8a44be931770f84d815
pa3datka <pa3datka@gmail.com>
2023-07-11 21:25:34 +0300

    TMT-31: add marcup in page test invite

--- pages/profile/test/invite-[id].vue
+++ pages/profile/test/invite-[id].vue
@@ -30,10 +30,13 @@ const data = reactive({
 
       <template v-slot:content>
         <div class="invite-container">
+
           <div class="settings-view pt-sm-30 pt-lg-40 pl-sm-20 pl-md-30 pr-sm-20 pr-lg-30 pb-sm-30 pb-lg-40">
-            <Selection :id="'select-status-test'" name="staus-test" :list="data.statusTestList"/>
+            <div class="input-title">Кто может проходить тест</div>
+            <Selection class="pt-lg-30 pt-md-20 pt-sm-14" :id="'select-status-test'" name="staus-test" :list="data.statusTestList"/>
           </div>
-          <div class="settings-user">
+          <div class="settings-user pt-sm-30 pt-lg-40 pl-sm-20 pl-md-30 pr-sm-20 pr-lg-30 pb-sm-30 pb-lg-40">
+            asd
           </div>
         </div>
       </template>
@@ -43,10 +46,59 @@ const data = reactive({
 </template>
 
 <style lang="scss">
+@import "assets/css/variables.scss";
 .page-test-invite {
   .invite-container {
+    display: grid;
+    grid-template-areas: 'users' 'views';
     .settings-view {
+      grid-area: views;
 
+      .input-title {
+        color: $color-black-market;
+        font-size: $font15;
+        font-weight: $font-weight5;
+      }
+    }
+
+    .settings-user {
+      grid-area: users;
+      background-color: $color-white-solid;
+      border-bottom-right-radius: rem-calc(18);
+      margin-top: rem-calc(5);
+    }
+  }
+}
+
+@media (min-width: rem-calc(768)) {
+  .page-test-invite {
+    .invite-container {
+      grid-template-areas: 'views users';
+      grid-template-columns: 50% 50%;
+      .settings-view {
+        .input-title {
+          font-size: $font17;
+        }
+      }
+
+      .settings-user {
+        margin-top: rem-calc(5);
+      }
+    }
+  }
+}
+
+@media (min-width: rem-calc(992)) {
+  .page-test-invite {
+    .invite-container {
+      grid-template-columns: 60% 40%;
+      .settings-view {
+
+      }
+
+      .settings-user {
+        margin-top: rem-calc(14);
+      }
     }
   }
 }
